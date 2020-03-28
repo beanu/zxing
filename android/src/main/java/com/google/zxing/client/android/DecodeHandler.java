c4eadb4dc77939434857aad7f37beabef213ee95
beanu <zhaoyunhello@gmail.com>
2020-03-28 15:48:32 +0800

    改为默认竖屏扫码

--- android/src/main/java/com/google/zxing/client/android/DecodeHandler.java
+++ android/src/main/java/com/google/zxing/client/android/DecodeHandler.java
@@ -140,6 +140,20 @@ final class DecodeHandler extends Handler {
     private void decode(byte[] data, int width, int height) {
         long start = System.nanoTime();
         Result rawResult = null;
+
+        //竖屏新增
+        byte[] rotatedData = new byte[data.length];
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                rotatedData[x * height + height - y - 1] = data[x + y * width];
+            }
+        }
+        int tmp = width; // Here we are swapping, that's the difference to #11
+        width = height;
+        height = tmp;
+        data = rotatedData;
+        //竖屏新增
+
         PlanarYUVLuminanceSource source = activity.getCameraManager().buildLuminanceSource(data, width, height);
         if (source != null) {
             BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
