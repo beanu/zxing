76e2786b51e8f8fe0cdf37a3d08f1069368ed75e
beanu <zhaoyunhello@gmail.com>
2020-03-30 10:47:01 +0800

    缩短聚焦的时间

--- android/src/main/java/com/google/zxing/client/android/camera/CameraManager.java
+++ android/src/main/java/com/google/zxing/client/android/camera/CameraManager.java
@@ -234,7 +234,7 @@ public final class CameraManager {
             int height = findDesiredDimensionInRange(screenResolution.y, MIN_FRAME_HEIGHT, MAX_FRAME_HEIGHT);
 
             int leftOffset = (screenResolution.x - width) / 2;
-            int topOffset = (screenResolution.y - height) / 2;
+            int topOffset = (screenResolution.y - height - 250) / 2;
             framingRect = new Rect(leftOffset, topOffset, leftOffset + width, topOffset + height);
             Log.d(TAG, "Calculated framing rect: " + framingRect);
         }
